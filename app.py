61360a75bb33bdc02afb39544164bd7fbf7e920a
hshetty2211 <125392882+hshetty2211@users.noreply.github.com>
2023-02-14 14:04:03 -0800

    Add min, max values for Dates and Shift inputs to sidebar

--- app.py
+++ app.py
@@ -4,14 +4,14 @@ import streamlit as st
 st.write("App Working!")
 
 tickers = ["AAPL", "CSCO", "DIS", "GE", "GS", "IBM", "JNJ", "JPM", "MCD", "MMM", "NKE", "PG"]
-ticker = st.selectbox("Pick a ticker", tickers)
+ticker = st..sidebar.selectbox("Pick a ticker", tickers)
 
 df = pd.read_csv(ticker + ".csv", parse_dates=["Date"], index_col=["Date"])
 
 beg_date = df.index.min()
 end_date = df.index.max()
 
-pick_start = st.date_input("Pick Start Date:", beg_date)
-pick_end = st.date_input("Pick End Date:", end_date)
+pick_start = st.sidebar.date_input("Pick Start Date:", beg_date, min_value = beg_date, max_value = end_date)
+pick_end = st.sidebar.date_input("Pick End Date:", end_date, min_value = beg_date, max_value = end_date)
 
 st.write(df.loc[pick_start:pick_end])
