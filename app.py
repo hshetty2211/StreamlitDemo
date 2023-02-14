345029c252bc358e2eb841ef16c1f35198bfffe2
hshetty2211 <125392882+hshetty2211@users.noreply.github.com>
2023-02-14 14:04:23 -0800

    Remove error in sidebar code

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import streamlit as st
 st.write("App Working!")
 
 tickers = ["AAPL", "CSCO", "DIS", "GE", "GS", "IBM", "JNJ", "JPM", "MCD", "MMM", "NKE", "PG"]
-ticker = st..sidebar.selectbox("Pick a ticker", tickers)
+ticker = st.sidebar.selectbox("Pick a ticker", tickers)
 
 df = pd.read_csv(ticker + ".csv", parse_dates=["Date"], index_col=["Date"])
 
