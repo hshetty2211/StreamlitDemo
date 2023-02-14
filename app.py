0ae30ab503af96d6f53a4929c8c3a0252b162209
hshetty2211 <125392882+hshetty2211@users.noreply.github.com>
2023-02-14 13:57:29 -0800

    Add Dates filter

--- app.py
+++ app.py
@@ -6,6 +6,12 @@ st.write("App Working!")
 tickers = ["AAPL", "CSCO", "DIS", "GE", "GS", "IBM", "JNJ", "JPM", "MCD", "MMM", "NKE", "PG"]
 ticker = st.selectbox("Pick a ticker", tickers)
 
-df = pd.read_csv(ticker + ".csv", parse_dates=["Date"])
+df = pd.read_csv(ticker + ".csv", parse_dates=["Date"], index_col=["Date"])
 
-st.write(df)
+beg_date = df.index.min()
+end_date = df.index.max()
+
+pick_start = st.date_input("Pick Start Date:", beg_date)
+pick_end = st.date_input("Pick End Date:", end_date)
+
+st.write(df.loc[pick_start:pick_end])
