0e554e445f56ce7ab98ad887f6dd76894a5a7807
hshetty2211 <125392882+hshetty2211@users.noreply.github.com>
2023-02-14 16:00:20 -0800

    Delete Assignment 2.py

--- Assignment 2.py
+++
@@ -1,7 +0,0 @@
-#@st.cache
-def fin_data():
-    df = pd.read_excel("ExData/Data Manipulation Worksheet.xlsx", sheet_name = "Financing Table Clean", 
-                    parse_dates=["DATE"], index_col=[0])
-    return df
-
-finDeals = fin_data () 
