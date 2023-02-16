e6524515eb247404992bba972f160dc68336f10b
Harshit Shetty <125392882+hshetty2211@users.noreply.github.com>
2023-02-16 11:24:44 -0800

    Clean code in Spyder, add comments and fix UNIX error

--- YahooWebScrape.py
+++ YahooWebScrape.py
@@ -4,7 +4,7 @@ This document scrapes yahoo finance for stock ticker historical prices
 
 Author: Harshit Shetty 
 
-Date: 14 Feb 2023
+Date: Feb 2023
 
 """
 
@@ -14,47 +14,52 @@ import numpy as np
 import pandas as pd
 import requests
 import streamlit as st
-import datetime 
+import datetime
+import time
+import math 
 import plotly.express as px
 
 #%% Web Scrape
 
-ticker = st.sidebar.text_input("Which stock ticker do you want to analyse?", "AAPL")
+# Select Ticker
+ticker = st.sidebar.text_input("Which stock ticker do you want to analyse?", "GOOG")
 
+# Select Date Range
+
+# Default Date Ranges
 today = datetime.date.today()
-one_day = datetime.timedelta(days=1)
-end_date = today - one_day
+one_year = datetime.timedelta(days=366)
+start_date = today - one_year # One year ago
+one_day = datetime.timedelta(days=1) 
+end_date = today - one_day # Yesterday
 
-start_time = st.sidebar.date_input("Pick Start Date:")
+# Date Inputs
+start_time = st.sidebar.date_input("Pick Start Date:", start_date)
 end_time =  st.sidebar.date_input("Pick End Date:", end_date)
 
-#def stock_data(ticker, start_time, end_time):
+# Convert Dates to Unix
+start_time = time.mktime(start_time.timetuple())
+add_start_time = start_time + 1954.0
+start_time = math.trunc(add_start_time)
+start_time = str(start_time)
 
-start_time = datetime.datetime.combine(start_time, datetime.datetime.min.time())
-epoch_start_time = str(int(start_time.timestamp()))
+end_time = time.mktime(end_time.timetuple())
+add_end_time = end_time + 1954 
+end_time = math.trunc(add_end_time)
+end_time = str(end_time)
 
-end_time = datetime.datetime.combine(end_time, datetime.datetime.min.time())
-epoch_end_time = str(int(start_time.timestamp()))
-    
-url = "https://query1.finance.yahoo.com/v7/finance/download/" + ticker + "?period1=" + epoch_start_time + "&" + epoch_end_time + "&interval=1d&events=history&includeAdjustedClose=true"
+# Build URL and Web Scrape to dataframe
+url = "https://query1.finance.yahoo.com/v7/finance/download/" + ticker + "?period1=" + start_time + "&period2=" + end_time + "&interval=1d&events=history&includeAdjustedClose=true"
 
 user = {'User-agent':'Mozilla/5.0'}    
 site = requests.get(url, headers=user)
-df = pd.read_csv(site.text)
-filter_df = pd.DataFrame()
-df_prices = pd.DataFrame(df.iloc[5])
+df = pd.read_csv(url, parse_dates= ["Date"])
 
-df_daily_returns = np.log(df_prices).diff()
+# Extract price data for analysis
+df_prices = df[["Date","Adj Close"]]
+df_prices = df_prices.set_index("Date")
 
-df_daily_mean = df_daily_returns.mean(axis=0)
-
-daily_returns = df_daily_returns.to_numpy()
-exp_ret = df_daily_mean.to_numpy()
-
-fig = px.line(filter_df, x=filter_df.index, y="Adj Close")
+# Plot price data
+fig = px.line(df_prices, x=df_prices.index, y="Adj Close")
 st.plotly_chart(fig, use_container_width = True)
-
-    #return df_prices, daily_returns, exp_ret
-
-
 
